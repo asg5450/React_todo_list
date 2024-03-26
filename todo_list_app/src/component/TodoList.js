c05b5748fbe5d102f679eeee8ee7e5579efda779
asg5450 <asg5450@gmail.com>
2024-03-26 12:11:05 +0900

    TodoItem Component 생성
    TodoItem Component 생성

--- todo_list_app/src/component/TodoList.js
+++ todo_list_app/src/component/TodoList.js
@@ -1,10 +1,14 @@
 import "./TodoList.css";
+import TodoItem from "./TodoItem.js";
 
 const TodoList = () => {
   return (
     <div className="TodoList">
       <h4>Todo List 🌱</h4>
       <input className="searchbar" placeholder="검색어를 입력하세요" />
+      <div className="list_wrapper">
+        <TodoItem />
+      </div>
     </div>
   );
 };
