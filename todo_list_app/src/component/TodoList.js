8b4011ecf80a840ab28dea138e1e118931c7f96f
asg5450 <asg5450@gmail.com>
2024-03-28 13:44:46 +0900

    배열 데이터 map으로 랜더링
    배열 데이터 map으로 랜더링

--- todo_list_app/src/component/TodoList.js
+++ todo_list_app/src/component/TodoList.js
@@ -1,13 +1,15 @@
-import "./TodoList.css";
 import TodoItem from "./TodoItem.js";
+import "./TodoList.css";
 
-const TodoList = () => {
+const TodoList = ({ todo }) => {
   return (
     <div className="TodoList">
       <h4>Todo List 🌱</h4>
       <input className="searchbar" placeholder="검색어를 입력하세요" />
       <div className="list_wrapper">
-        <TodoItem />
+        {todo.map((it) => (
+          <TodoItem {...it} />
+        ))}
       </div>
     </div>
   );
