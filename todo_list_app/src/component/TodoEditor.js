0d184a531811caf0a458683073fbe86f40e3dc23
asg5450 <asg5450@gmail.com>
2024-03-28 13:05:15 +0900

    useRef 참조 및 포커싱 활용
    useRef 참조 및 포커싱 활용

--- todo_list_app/src/component/TodoEditor.js
+++ todo_list_app/src/component/TodoEditor.js
@@ -1,26 +1,32 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./TodoEditor.css";
 
 const TodoEditor = ({ onCreate }) => {
   const [content, setContent] = useState("");
+  const inputRef = useRef();
   const onChangeContent = (e) => {
     setContent(e.target.value);
   };
 
-  // const onSubmit = () => {
-  //   onCreate(content);
-  // };
+  const onSubmit = () => {
+    if (!content) {
+      inputRef.current.focus();
+      return;
+    }
+    onCreate(content);
+  };
 
   return (
     <div className="TodoEditor">
       <h4>새로운 Todo 작성하기 ✏️</h4>
       <div className="editor_wrapper">
         <input
+          ref={inputRef}
           value={content}
           onChange={onChangeContent}
           placeholder="새로운 Todo ..."
         />
-        <button onClick={onCreate}>추가</button>
+        <button onClick={onSubmit}>추가</button>
       </div>
     </div>
   );
