f2da7e48baac76a50fcec83a5ba6ce354017961b
asg5450 <asg5450@gmail.com>
2024-03-28 12:32:42 +0900

    "할 일"을 state에 추가하기
    "할 일"을 state에 추가하기

--- todo_list_app/src/App.js
+++ todo_list_app/src/App.js
@@ -2,12 +2,48 @@ import "./App.css";
 import Header from "./component/Header.js";
 import TodoEditor from "./component/TodoEditor.js";
 import TodoList from "./component/TodoList.js";
+import { useState, useRef } from "react";
+
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createdDate: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todo, setTodo] = useState(mockTodo);
+  const idRef = useRef(todo.length);
+
+  const onCreate = (content) => {
+    const newItem = {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createDate: new Date().getTime(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  };
+
   return (
     <div className="App">
       <Header />
-      <TodoEditor />
+      <TodoEditor onCreate={onCreate} />
       <TodoList />
     </div>
   );
--- todo_list_app/src/component/TodoEditor.js
+++ todo_list_app/src/component/TodoEditor.js
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import "./TodoEditor.css";
 
-const TodoEditor = () => {
+const TodoEditor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  // const onSubmit = () => {
+  //   onCreate(content);
+  // };
+
   return (
     <div className="TodoEditor">
       <h4>새로운 Todo 작성하기 ✏️</h4>
       <div className="editor_wrapper">
-        <input placeholder="새로운 Todo ..." />
-        <button>추가</button>
+        <input
+          value={content}
+          onChange={onChangeContent}
+          placeholder="새로운 Todo ..."
+        />
+        <button onClick={onCreate}>추가</button>
       </div>
     </div>
   );
